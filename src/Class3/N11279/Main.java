4868f7b57ea74e16fb30de73c6c307a77b1ec4d8
tellang <tellang@daum.net>
2021-07-16 14:38:14 +0900

    backup

--- src/Class3/N11279/Main.java
+++ src/Class3/N11279/Main.java
@@ -3,8 +3,6 @@ package Class3.N11279;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
