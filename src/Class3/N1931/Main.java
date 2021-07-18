5fb07c3a3ccd46f3e3ee03781e2921de93834a40
tellang <tellang@daum.net>
2021-07-18 19:09:50 +0900

    시작과 끝이 같은 시간이 존재할때, 그것과 끝이 같은 길이가 1 이상인 가장 짧은 시간을 고려하지 않은 문제 해결

--- src/Class3/N1931/Main.java
+++ src/Class3/N1931/Main.java
@@ -13,22 +13,34 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> shortTimeCounter = new TreeMap<>();
         int N = Integer.parseInt(br.readLine());
+        int count = 0;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int exStart = map.getOrDefault(end, -1);
-            if (exStart == -1 && exStart < start) {
-                map.put(end, start);
+            if (end != start){
+                if (exStart == -1 || exStart < start) {
+                    map.put(end, start);
+                }else if (exStart == end){
+                    map.put(end, start);
+                    shortTimeCounter.put(end, shortTimeCounter.getOrDefault(end, 0)+1);
+                }
+            }else{
+                if (exStart == -1) {
+                    map.put(end, start);
+                }else
+                    shortTimeCounter.put(end, shortTimeCounter.getOrDefault(end, 0)+1);
             }
         }
         int lastEnd = -1;
-        int count = 0;
         for (var end:map.keySet()
         ) {
-            if (lastEnd <= map.get(end)){
-                count++;
+            int start = map.get(end);
+            if (lastEnd <= start){
+                count+= shortTimeCounter.getOrDefault(end, 0)+1;
                 lastEnd = end;
             }
         }
