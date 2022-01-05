01c9b7783200970114d2cdadf601f841343b78fe
tellang <tellang@daum.net>
2022-01-05 14:45:52 +0900

    대표 테스트케이스 통과

--- src/Class3/N5430/Main.java
+++ src/Class3/N5430/Main.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
@@ -19,12 +22,7 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             status = NORMAL;
-            Deque<String> dq = new LinkedList<>() {
-                @Override
-                public String toString() {
-                    return super.toString().trim();
-                }
-            };
+            Deque<String> dq = new LinkedList<>();
             var func = br.readLine();
             int n = Integer.parseInt(br.readLine());
             boolean hasError = false;
@@ -49,13 +47,24 @@ public class Main {
             if (hasError)
                 sb.append("error\n");
             else {
-//                if (status == NORMAL)
-//                    sb.append(dq).append("\n");
-//                else
-//                    sb.reverse().append(dq).append("\n").reverse();
-                sb.append(dq).append("\n");
+                if (status == NORMAL)
+                    sb.append(getString(dq.iterator())).append("\n");
+                else
+                    sb.append(getString(dq.descendingIterator())).append("\n");
             }
         }
         System.out.println(sb);
+    }
+    static String getString(Iterator<String> iterator) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        while (iterator.hasNext()){
+            sb.append(iterator.next());
+            if(iterator.hasNext())
+                sb.append(",");
+            else
+                sb.append("]");
+        }
+        return sb.toString();
     }
 }
